2b21db0ac9ef4f433b552d44041de62b2c334b9f
palRajan196 <palrajan196@gmail.com>
2025-02-18 15:41:49 +0530

    Push on git

---
+++ client/src/config/config.js
@@ -0,0 +1,7 @@
+import { io } from "socket.io-client";
+
+const URL = "http://localhost:5000";
+const Backend_URL = process.env.REACT_APP_BackEnd;
+
+export const socket = io(URL);
+export const navbarBrand = "YourVideoShare";
